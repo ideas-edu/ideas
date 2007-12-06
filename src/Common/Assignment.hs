355245e6688f8478b88fd6ecdb0c48180de3346b
Bastiaan Heeren <bastiaan.heeren@ou.nl>
2007-12-06 10:31:12 +0000

    First implementation of assignment: slightly changed parser type
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@737 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Common/Assignment.hs
+++ src/Common/Assignment.hs
@@ -1,13 +1,15 @@
+{-# OPTIONS -XTypeSynonymInstances #-}
 module Common.Assignment where
 
 import Common.Transformation
 import Common.Strategy
+import System.Random
 import Test.QuickCheck
 
 data Language = English | Dutch
 
 data Assignment a = Assignment
-   { parser        :: String -> a
+   { parser        :: String -> Either (Doc a, Maybe a) a
    , prettyPrinter :: a -> String
    , equivalence   :: a -> a -> Bool
    , finalProperty :: a -> Bool
@@ -18,35 +20,82 @@ data Assignment a = Assignment
    }
    
 randomTerm :: Assignment a -> IO a
-randomTerm = undefined
+randomTerm a = do 
+   stdgen <- newStdGen
+   return (randomTermWith stdgen a)
+
+-- | Default size is 100
+randomTermWith :: StdGen -> Assignment a -> a
+randomTermWith stdgen = generate 100 stdgen . generator
 
 -- | Returns a text and the rule that is applicable
 giveHint :: Assignment a -> a -> (Doc a, Rule a)
-giveHint = undefined
+giveHint x a =
+   case giveHints x a of
+      hd:_ -> hd
+      _    -> (emptyDoc, idRule)
 
+-- | Returns a text and the rule that is applicable
+giveHints :: Assignment a -> a -> [(Doc a, Rule a)]
+giveHints x = map g . nextRulesWith (not . isMinorRule) (strategy x)
+ where
+   g (rs, a, s) = (toDoc (name (last rs)), last rs) -- ignores rest strategy
+   
 -- | Returns a text, a sub-expression that can be rewritten, and the result
 -- | of the rewriting
 giveStep :: Assignment a -> a -> (Doc a, a, a)
-giveStep = undefined
+giveStep x a = 
+   case giveSteps x a of
+      hd:_ -> hd
+      _    -> (emptyDoc, a, a)
+
+giveSteps :: Assignment a -> a -> [(Doc a, a, a)]
+giveSteps x a = map g $ nextRulesWith (not . isMinorRule) (strategy x) a
+ where
+   g (rs, b, s) = (toDoc (name (last rs)), applyListD (init rs) a, b) -- ignores rest strategy
 
 feedback :: Assignment a -> a -> String -> Feedback a
-feedback = undefined
+feedback x a txt =
+   case parser x txt of
+      Left (msg, suggestion) -> 
+         SyntaxError msg suggestion
+      Right new
+         | equivalence x a new -> 
+              Correct (toDoc "Well done!") True -- strategy not yet considered
+         | otherwise -> 
+              Incorrect (toDoc "Incorrect") Nothing -- no suggestion yet
 
 stepsRemaining :: Assignment a -> a -> Int
-stepsRemaining = undefined
+stepsRemaining x a = 
+   case intermediates (strategy x) a of
+      (rs, _, _):_ -> length (filter (not . isMinorRule) rs)
+      _            -> 0
 
 data Feedback a = SyntaxError (Doc a) (Maybe a) {- corrected -}
                 | Incorrect   (Doc a) (Maybe a)
                 | Correct     (Doc a) Bool
                 
-data Doc a = DocText String
-           | DocDomain a
-           | DocConcat [Doc a]
+newtype Doc a = D [DocItem a]
+
+data DocItem a = Text String | Domain a
            
-instance Show a => Show (Doc a)
+instance Show a => Show (Doc a) where
+   show = showDocWith show
+
+emptyDoc :: Doc a
+emptyDoc = D []
 
 showDoc :: Assignment a -> Doc a -> String
-showDoc = undefined
+showDoc = showDocWith . prettyPrinter
 
 showDocWith :: (a -> String) -> Doc a -> String
-showDocWith = undefined
+showDocWith f (D xs) = concatMap g xs
+ where
+   g (Text s)   = s
+   g (Domain a) = f a 
+   
+class IsDoc a where
+   toDoc :: a -> Doc b
+
+instance IsDoc String where
+   toDoc s = D [Text s]

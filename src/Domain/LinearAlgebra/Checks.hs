76cc0de7ea5be9f0bf3ee7dd5b39be35b674077f
Bastiaan Heeren <bastiaan.heeren@ou.nl>
2008-01-04 11:17:04 +0000

    More realistic generator for matrices
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@780 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Domain/LinearAlgebra/Checks.hs
+++ src/Domain/LinearAlgebra/Checks.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS -XGeneralizedNewtypeDeriving #-}
 -----------------------------------------------------------------------------
 -- |
 -- Maintainer  :  bastiaan.heeren@ou.nl
@@ -74,6 +75,26 @@ arbSolution m = do
        newMatrix = makeMatrix $ zipWith (++) (rows m) finalCol
    return (solution, newMatrix)
    
+simplifyMatrix :: (Ord a, Num a) => [a] -> Matrix a -> Gen (Matrix a)
+simplifyMatrix solution m = do
+   rs <- mapM simplifyRow (rows m)
+   return (makeMatrix rs)
+ where
+   make xs  = xs ++ [sum $ zipWith (*) solution xs]
+   f []     = []
+   f (x:xs) = map (:xs) (g x) ++ map (x:) (f xs)
+   g x      = filter (/=0) [x-1, x+1, negate x]
+   simplifyRow r
+      | x > 5 = 
+           case filter ((< x) . abs . last) $ map make $ f xs of
+              []   -> return r
+              list -> oneof (map return list) >>= simplifyRow
+      | otherwise = 
+           return r
+    where 
+       x  = abs (last r)
+       xs = init r
+   
 -----------------------
 -- Assignments: temporarily
 
@@ -85,10 +106,23 @@ reduceMatrixAssignment = makeAssignment
    , equivalence   = \x y -> applyD toReducedEchelon (inContext $ matrix x) == applyD toReducedEchelon (inContext $ matrix y)
    , ruleset       = matrixRules
    , finalProperty = inRowReducedEchelonForm . matrix
-   , generator     = liftM (inContext . fmap fromInteger) (arbSizedMatrix (4, 3))
+   , generator     = do m1        <- arbSizedMatrix (3, 3)
+                        (sol, m2) <- arbSolution m1
+                        m3        <- simplifyMatrix sol m2
+                        return $ inContext $ fmap fromSmallInt m3
    , strategy      = toReducedEchelon
    }
 
 instance RealFrac a => Arbitrary (MatrixInContext a) where
    arbitrary = liftM (inContext . fmap fromInteger) (arbitrary)
    coarbitrary mic = coarbitrary (fmap round $ matrix mic :: Matrix Integer)
+   
+newtype SmallInt = SmallInt Int
+   deriving (Show, Eq, Ord, Num)
+
+fromSmallInt :: Num a => SmallInt -> a
+fromSmallInt (SmallInt n) = fromIntegral n
+
+instance Arbitrary SmallInt where
+   arbitrary = oneof $ map (return . SmallInt) [-15 .. 15]
+   coarbitrary (SmallInt n) = coarbitrary n

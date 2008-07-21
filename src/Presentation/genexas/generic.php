b0162bdbbfcd0886a60cc931e0d51ecb1c9de8be
Sylvia Stuurman <sylvia.stuurman@ou.nl>
2008-07-21 14:18:30 +0000

    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@1216 d9a34ac6-6904-0410-8848-93d71bbb9804

---
+++ src/Presentation/genexas/generic.php
@@ -0,0 +1,13 @@
+<?php
+function rules() {
+	include_once("common/en/rules.html");
+}
+function help() {
+	include_once("common/en/help.html");
+}
+function about() {
+	include_once("common/en/about.html");
+}
+include_once("common/en.php");
+include_once("common/generic.php");
+?>

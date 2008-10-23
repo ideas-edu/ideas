1dfacc7060c99d849306062e2eaebbc24b09f6fa
Alex Gerdes <agerdes@me.com>
2008-10-23 14:06:11 +0000

    Expr parser
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@1350 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Domain/Programming/Parser.hs
+++ src/Domain/Programming/Parser.hs
@@ -3,6 +3,7 @@ module Domain.Programming.Parser where
 import Common.Parsing
 import Domain.Programming.Expr
 import Domain.Programming.Prelude
+import Domain.Programming.Eval
 import Data.Char
 
 -----------------------------------------------------------
@@ -16,8 +17,11 @@ import Data.Char
 
 scannerExpr :: Scanner
 scannerExpr = defaultScanner
-   { keywords         = ["False", "case", "of", "if", "then", "else"] 
-   , keywordOperators = ["->", "\\"]
+   { keywords         = [ "case", "of"
+                        , "if", "then", "else"
+                        , "let", "in"
+                        ] 
+   , keywordOperators = ["->", "\\", "="]
    }
 
 -----------------------------------------------------------
@@ -30,34 +34,38 @@ parseExpr = f . parse pExpr . scanWith scannerExpr
    f (_, xs) = Left $ ErrorMessage $ unlines $ map show xs
    
 pExpr :: TokenParser Expr
-pExpr = foldl1 (<|>) [pLambda, pVar, pApply, pFix, pInt, pIfThenElse, pMatchList]
+pExpr = pLambda <|> pOps
 
 pLambda :: TokenParser Expr
-pLambda = Lambda <$ pKey "\\" <*> pString <* pKey "->" <*> pExpr
+pLambda = lambda <$ pKey "\\" <*> pVarid <* pKey "->" <*> pExpr
+  where 
+    lambda x e = Lambda (fst x) e
 
-pVar :: TokenParser Expr
-pVar = (Var . fst) <$> pVarid
+pOps :: TokenParser Expr
+pOps = pApply -- if any ops are introduced: pChainl O1 (pChainl O2 ... (pChainl On pApply)
 
 pApply :: TokenParser Expr
-pApply = Apply <$> pExpr <*> pExpr
+pApply = app <$> pList1 pAtom
+ where 
+   app ats = case ats of 
+                  [at] -> at
+                  _    -> foldl1 Apply ats
 
-pFix :: TokenParser Expr
-pFix  = Fix <$> pExpr
-
-pInt :: TokenParser Expr
-pInt = (Int . fromInteger) <$> pInteger
-
-pIfThenElse :: TokenParser Expr
-pIfThenElse = IfThenElse <$> pExpr <*> pExpr <*> pExpr
-
-pMatchList:: TokenParser Expr
-pMatchList = MatchList <$> pExpr <*> pExpr <*> pExpr
-
-{-
-myParens :: TokenParser a -> TokenParser a
-myParens p = pSpec '(' *> p <* pSpec ')'
-
--}
+pAtom :: TokenParser Expr
+pAtom  =  (Var . fst) <$> pVarid
+      <|> (\(f,_) e body -> Apply (Lambda f body) (Fix $ Lambda f e))  -- Fix
+              <$ pKey "let" <*> pVarid
+              <* pKey "="   <*> pExpr
+              <* pKey "in"  <*> pExpr
+      <|> (Int . fromInteger) <$> pInteger       -- Int
+      <|> pSpec '(' *> pExpr <* pSpec ')'        -- parantheses
+      <|> IfThenElse <$ pKey "if"   <*> pExpr    -- If
+                     <* pKey "then" <*> pExpr 
+                     <* pKey "else" <*> pExpr
+      <|> MatchList <$  pKey "case" <*> pExpr    -- case
+                    <*  pKey "of"   
+                             <*> pAtom              -- nil
+                             <*  pSpec ';' <*> pAtom -- cons
 
 -----------------------------------------------------------
 --- Pretty-Printer
@@ -67,6 +75,7 @@ pprintExpr (expr, offset) =
    case expr of
       Lambda x e -> "\\" ++ x ++ " -> " ++ pprintExpr (e,offset+(length x)+5)
       Var x -> x
+      --Apply (Lambda f body) (Fix $ Lambda f e))
       Apply f a -> pprintExpr (f,offset) ++ case a of
                                                (Var _)   -> " "  ++ pprintExpr (a,offset)
                                                _         -> " (" ++ pprintExpr (a,offset) ++ ")"
@@ -79,8 +88,6 @@ pprintExpr (expr, offset) =
       MatchList b n c -> "case " ++ pprintExpr (b,offset) ++ " of\n" 
                                  ++ spaces (offset+5) ++ pprintExpr (n,offset+5) ++ "\n"
                                  ++ spaces (offset+5) ++ pprintExpr (c,offset+5)
---      Let x b d -> "let " ++ x ++ " = " ++ pprintExpr (b,offset+(length x)+7) ++ "\n" ++ spaces offset ++
---                   "in " ++ pprintExpr (d,offset)
      where 
       spaces n = take n (Prelude.repeat ' ')
 

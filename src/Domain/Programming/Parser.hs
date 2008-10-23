59676f6e06b13189593443fc65afd8611293b8cb
Alex Gerdes <agerdes@me.com>
2008-10-23 22:10:29 +0000

    Fixpoint fixed ;-)
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@1353 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Domain/Programming/Parser.hs
+++ src/Domain/Programming/Parser.hs
@@ -36,7 +36,7 @@ pLambda = lambda <$ pKey "\\" <*> pVarid <* pKey "->" <*> pExpr
   where 
     lambda x e = Lambda (fst x) e
 
-pOps :: TokenParser Expr -- if any ops are introduced: pChainl O1 (pChainl O2 ... (pChainl On pApply)
+pOps :: TokenParser Expr 
 pOps = flip ($) <$> pApply <*> optional ((\op r l -> Apply (Apply (Var op) l) r) <$> pVarsym <*> pApply) id
 
 pApply :: TokenParser Expr
@@ -51,9 +51,9 @@ pAtom  =  (Var . fst) <$> (pVarid <|> pConid)
       <|> (\(f,_) e body -> Apply (Lambda f body) (Fix $ Lambda f e))  -- Fix
               <$ pKey "let" <*> pVarid
               <* pKey "="   <*> pExpr
-              <* pKey "in"  <*> pExpr
+              <* pKey "in"  <*> pExpr 
       <|> (Int . fromInteger) <$> pInteger       -- Int
-      <|> pSpec '(' *> pExpr <* pSpec ')'        -- parantheses
+      <|> pSpec '(' *> pExpr <* pSpec ')'        -- parentheses
       <|> IfThenElse <$ pKey "if"   <*> pExpr    -- If
                      <* pKey "then" <*> pExpr 
                      <* pKey "else" <*> pExpr
@@ -70,13 +70,22 @@ pprintExpr (expr, offset) =
    case expr of
       Lambda x e -> "\\" ++ x ++ " -> " ++ pprintExpr (e,offset+(length x)+5)
       Var x -> x
-      Apply (Lambda f body) (Fix (Lambda _ e)) -> "let " ++ f ++ " = " ++ pprintExpr (e,offset)
+      Apply (Lambda f body) (Fix (Lambda _ e)) -> "let " ++ f ++ " = " ++ pprintExpr (e,offset+(length f)+7)
                                                   ++ "\n" ++ spaces offset ++ "in " ++ pprintExpr (body, offset)
-      Apply f a -> pprintExpr (f,offset) ++ case a of
+      Apply f a -> "(" ++ case f of
+                        (Apply (Var op) l) -> if not (isAlpha (head op)) then 
+                                                  pprintExpr (l,offset) ++ " " ++ op ++ " " ++ pprintExpr (a,offset)
+                                              else
+                                                  op ++ " " ++ pprintExpr (l,offset) ++ " " ++ pprintExpr (a,offset)
+                        _ -> pprintExpr (f,offset) ++ case a of
                                                (Var _)   -> " "  ++ pprintExpr (a,offset)
                                                (Int _)   -> " "  ++ pprintExpr (a,offset)
                                                _         -> " (" ++ pprintExpr (a,offset) ++ ")"
-      Fix f -> pprintExpr (f,offset)
+                   ++ ")"
+      Fix f -> case f of 
+                 (Lambda x e) -> "let " ++ x ++ " = " ++ pprintExpr (e,offset+(length x)+7) ++ "\n"
+                                 ++ spaces offset ++ "in " ++ x
+                 _ -> error "Must be a function"
       Int n -> [intToDigit n]
       IfThenElse c t e -> "if " ++ pprintExpr (c,offset) ++ " then\n"
                                 ++ spaces (offset+2) ++ pprintExpr (t,offset+2) ++ "\n"
@@ -90,4 +99,7 @@ pprintExpr (expr, offset) =
 
 
 prettyIsort = putStr (pprintExpr (isortE,0) ++ "\n") 
--- testje = "let f=(\\x->x) in f 1"
+
+testje = eval (isPermE # mylist # (isort # mylist))
+  where
+    isort = (\(Right e) -> e) $ parseExpr $ pprintExpr (isortE,0)

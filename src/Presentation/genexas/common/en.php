385655bd8dc6125ae29f7d3293f8189ee9e132e9
Bastiaan Heeren <bastiaan.heeren@ou.nl>
2009-03-12 13:38:58 +0000

    changed texts
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@1711 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Presentation/genexas/common/en.php
+++ src/Presentation/genexas/common/en.php
@@ -1,26 +1,26 @@
 <?php
 define("About", "About");
 define("Back", "Back");
-define("Clear", "Clear Feedback");
-define("ChooseClear", "Only show current feedback");
-define("ChooseKeep", "Show all feedback");
+define("Clear", "Clear area");
+define("ChooseClear", "Last message only");
+define("ChooseKeep", "All messages");
 define("Close", "Close");
 define("Copy", "Copy");
-define("Derivation", "Show worked out Example");
+define("Derivation", "Show worked-out example");
 define("Exercise", "Exercise");
 define("Feedback", "Feedback");
 define("Forward", "Forward");
 define("Help", "Help");
 define("Hint", "Hint");
 define("History", "Derivation");
-define("NewExercise", "New Exercise");
+define("NewExercise", "New exercise");
 define("Progress", "Progress");
 define("Ready", "Ready");
-define("Rules", "Rewrite Rules");
+define("Rules", "Rewrite rules");
 define("Step", "Step");
 define("Submit", "Submit");
-define("Welcome", "<p>Welcome to the Exercise Assistent. <br>Rewrite the expression in the Work area and hit Submit to start.</p><p>The Help button will tell you more.</p><p>The Feedback area will be cleared before new feedback is written to the area. If you want to keep all feedback in view, choose <q>Show all feedback</q> above.</p>");
-define("WorkArea", "Working Area: Rewrite and Submit");
+define("Welcome", "<p>Welcome to the Exercise Assistent. <br>Rewrite the expression in the work area and hit submit to start.</p><p>The Help button will tell you more.</p><p>The Feedback area will be cleared before new feedback is written to the area. If you want to keep all feedback in view, choose <q>Show all feedback</q> above.</p>");
+define("WorkArea", "Working area: rewrite and submit");
 function getLanguage() {
 	return "/genexas/common/javascript/en.js";
 }

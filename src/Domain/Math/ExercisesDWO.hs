9c6eb36d008c685723c3e8961291ee30bd16e414
Bastiaan Heeren <bastiaan.heeren@ou.nl>
2009-03-24 09:55:09 +0000

    Three new categories of exercise sets, borrowed from the DWO environment
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@1764 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Domain/Math/ExercisesDWO.hs
+++ src/Domain/Math/ExercisesDWO.hs
@@ -1,10 +1,118 @@
 module Domain.Math.ExercisesDWO
-   ( linearEquations, higherDegreeEquations ) where
+   ( calculateResults, fillInResult
+   , coverUpEquations, linearEquations, higherDegreeEquations 
+   ) where
 
 import Prelude hiding ((^))
 import Domain.Math.Equation
 import Domain.Math.Expr
 import Domain.Math.Symbolic
+
+calculateResults :: [[Expr]]
+calculateResults = [level1, level2, level3]
+ where
+   level1 = 
+      [ -8*(-3)
+      , -3-9
+      , 55/(-5)
+      , -6*9
+      , -11- (-3)
+      , 6-(-9)
+      , -10+3
+      , 6+(-5)
+      ]
+      
+   level2 = 
+      [ -3-(6*(-3))
+      , -12/3 - 3
+      , -4*(2+3)
+      , 2-6*6
+      , -27/(4-(-5))
+      , (-24/(-6)) - 3
+      , 8-(-77/(-11))
+      , 4/(-4+5)
+      ]
+      
+   level3 = 
+      [ 4*(3-(6-2))
+      , (-16-9)/5 - 3
+      , 4- (4-13)/(-3)
+      , (3*(-3))-5-4
+      , -55/(3*(-5)+4)
+      , -4*(-2+ (-4)+7)
+      , -8 - (140/4*5)
+      , 13-(2-1) / 3
+      ]
+
+fillInResult :: [[Equation Expr]]
+fillInResult = [level1, level2, level3]
+ where
+   level1 = 
+      let x = variable "x" in
+      [ x-2    :==: 2
+      , -4*x   :==: -28
+      , -8*x   :==: 72
+      , x+4    :==: 09
+      , 4+x    :==: 2
+      , -10-x  :==: -7
+      , x/(-8) :==: -3
+      , 11-x   :==: 14
+      ]
+      
+   level2 = 
+      let x = variable "x" in
+      [ -5-3*x      :==: -23
+      , 21/x - 4    :==: 3
+      , -3*(x+3)    :==: -27
+      , 2-5*x       :==: 47
+      , 18/(7-x)    :==: 6
+      , -77/x  + 4  :==: -7
+      , -7-(x/(-5)) :==: -15
+      , -18/(-3+x)  :==: 3
+      ]
+
+   level3 = 
+      let x = variable "x" in
+      [ -5*(5-(3-x))    :==: -20
+      , (-20-x)/(-5)-2  :==: 3
+      , 4-(x-14)/(-3)   :==: 1
+      , 3*x - 3 - 7     :==: 8
+      , -42/(-2*x+2)    :==: 7
+      , 3*(4+x+2)       :==: 12
+      , -6-(-54/(-3*x)) :==: -12
+      , 14-(x-3)/4      :==: 3
+      ]
+
+coverUpEquations :: [[Equation Expr]]
+coverUpEquations = [level1, level2]
+ where
+   level1 = 
+      let x = variable "x" in
+      [ 38-7*x       :==: 3
+      , sqrt (125/x) :==: 5
+      , 4*(12-x) + 7 :==: 35
+      , 5*x^2        :==: 80 
+      , 5*(5-x)      :==: 35
+      , 32/sqrt x    :==: 8
+      , (21/x)-8     :==: -1
+      , 180/x^2      :==: 5
+      , 3*(x-8)^2    :==: 12
+      , (8-x)/3 + 7  :==: 9
+      ]
+   
+   level2 = 
+      let x = variable "x" in
+      [ sqrt (x+9)/2       :==: 3
+      , (4*x-18)^2         :==: 4
+      , 3*(13-2*x)^2 - 20  :==: 55
+      , 5*((x/3) - 8)^2    :==: 20
+      , (6/(sqrt (x-7)))^3 :==: 8
+      , 8-(15/(sqrt (31-x)))         :==: 5
+      , sqrt (4*(x^2-21))            :==: 4
+      , 3 + (44/(sqrt (87 + x)))     :==: 7
+      , 13-(56 / (21 + (70/(3+x))))  :==: 12
+      , 12/(2+(24/(8+(28/(2+9/x))))) :==: 3
+      ]  
 
 linearEquations :: [[Equation Expr]]
 linearEquations = [level1, level2, level3, level4, level5]

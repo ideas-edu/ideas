b618ef0ea28a1865b6829583b75ac6f84246d758
Alex Gerdes <agerdes@me.com>
2009-08-26 12:34:41 +0000

    Work on inlining
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@2062 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Domain/Programming/Prog.hs
+++ src/Domain/Programming/Prog.hs
@@ -25,7 +25,9 @@ import Data.List hiding (union, lookup)
 import Domain.Programming.AlphaRenaming (alphaRenaming)
 import Domain.Programming.Helium
 import Domain.Programming.HeliumRules
+import Domain.Programming.InlinePatternBindings (inlinePatternBindings)
 import Domain.Programming.Substitute
+import Domain.Programming.Utils
 
 {- ideas:
 
@@ -75,42 +77,6 @@ checkExercises strat es = do
   let percentage = count * 100  `div` toInteger (length es)
   putStrLn $ "\n" ++ take 4 (show percentage) ++ "% has been recognised by the strategy.\n"
   return ()
-
-pat2expr :: Pattern -> Expression
-pat2expr p = 
-  case p of 
-    Pattern_Literal r l                -> Expression_Literal r l
-    Pattern_Variable r n               -> Expression_Variable r n
-    Pattern_Constructor r n _          -> Expression_Constructor r n
-    Pattern_Parenthesized r p          -> Expression_Parenthesized r $ pat2expr p
-    Pattern_InfixConstructor r' l op r -> Expression_InfixApplication r'
-                                            (MaybeExpression_Just (pat2expr l))
-                                            (var op)
-                                            (MaybeExpression_Just (pat2expr r))
-    Pattern_List r ps                  -> Expression_List r $ map pat2expr ps
-
-expr2pat e = 
-  case e of
-    Expression_Literal r l                -> Pattern_Literal r l
-    Expression_Variable r n               -> Pattern_Variable r n
-    Expression_Constructor r n            -> Pattern_Constructor r n []
-    Expression_Parenthesized r e          -> Pattern_Parenthesized r $ expr2pat e
---    Expression_InfixApplication r' l op r -> Pattern_InfixApplication r'
---                                               (MaybeExpression_Just (pat2expr l))
---                                               ( op)
---                                               (MaybeExpression_Just (pat2expr r))
-    Expression_List r es                  -> Pattern_List r $ map expr2pat es
-         
-
-var = Expression_Variable noRange
-pat = Pattern_Variable noRange
-patBinding pat expr w = Declaration_PatternBinding noRange pat $ 
-                          RightHandSide_Expression noRange expr w
-lambda ps expr = Expression_Lambda noRange ps expr
-letItBe ds expr = Expression_Let noRange ds expr
-
-pp = putStrLn . ppModule
-comp = (\(Right m)->m) . compile . fst
 
 --------------------------------------------------------------------------------
 -- Equality: rewrite to normal form and check syntatically (cannot be solved generally)

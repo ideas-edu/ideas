bfbfe81240f56bfd7db71711cc108e18fff2f24b
Bastiaan Heeren <bastiaan.heeren@ou.nl>
2009-12-04 13:44:04 +0000

    Moving GenExas folder to root of our IDEAS svn repo
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@2573 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Presentation/genexas/handlelogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-	global $studentid;
-	$_SESSION["oustudent"] = $_POST['naam'];
-	setcookie('oustudent', $_SESSION["oustudent"], 60*60*24*60, '/genexas/');
-	$studentid = $_COOKIE['oustudent'];
-	Header("Location: ./index.php");
-	exit();
-?>

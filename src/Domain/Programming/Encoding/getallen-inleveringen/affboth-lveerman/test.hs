5c11f0a64153c8d6d8798f6f8be2cb6743a810fe
Alex Gerdes <agerdes@me.com>
2009-12-10 21:02:52 +0000

    removed student solutions from repo
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@2592 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Domain/Programming/Encoding/getallen-inleveringen/affboth-lveerman/test.hs
+++
@@ -1,6 +0,0 @@
-fromBase' :: Int -> [Int] -> Int
-fromBase' _ []     = 0
-fromBase' n (x:xs) = x * n ^ length xs + fromBase' n xs
-
-fromBin :: [Int] -> Int
-fromBin = fromBase' 2
--- src/Domain/Programming/HeliumCoreInstances.hs
+++ src/Domain/Programming/HeliumCoreInstances.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE StandaloneDeriving, DeriveDataTypeable, TypeSynonymInstances #-}
+{-# LANGUAGE StandaloneDeriving, DeriveDataTypeable #-}
 
 ---------------------------------------------------------------------------
 -- Copyright 2009, Open Universiteit Nederland. This file is distributed 
@@ -16,9 +16,8 @@
 
 module Domain.Programming.HeliumCoreInstances where
 
+import Data.Data
 import Core
-import IdMap
-import IntMap
 
 deriving instance Show Expr
 deriving instance Show Note
@@ -28,12 +27,21 @@ deriving instance Show Alt
 deriving instance Show Pat
 deriving instance Show Literal
 deriving instance Show a => Show (Con a) 
-deriving instance Show a => Show (Module a)
-deriving instance Show a => Show (Decl a)
-deriving instance Show Custom
-deriving instance Show Access
 
-instance Show (IdMap a) where
-  show _ = ""
-instance Show (IntMap a) where
-  show _ = ""
+deriving instance Data Expr
+deriving instance Data Note
+deriving instance Data Binds
+deriving instance Data Bind
+deriving instance Data Alt
+deriving instance Data Pat
+deriving instance Data Literal
+deriving instance Data a => Data (Con a)
+
+deriving instance Typeable Expr
+deriving instance Typeable Note
+deriving instance Typeable Binds
+deriving instance Typeable Bind
+deriving instance Typeable Alt
+deriving instance Typeable Pat
+deriving instance Typeable Literal
+

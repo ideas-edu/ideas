6c32f9dcc858a94f083ec756bf405dac21620d5b
Bastiaan Heeren <bastiaan.heeren@ou.nl>
2010-02-02 09:32:55 +0000

    Slight improvement in productView: considering two special cases
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@2656 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Domain/Math/Expr/Views.hs
+++ src/Domain/Math/Expr/Views.hs
@@ -141,7 +141,10 @@ productView :: View Expr (Bool, [Expr])
 productView = makeView (Just . second ($ []) . f False) g
  where
    f r (a :*: b)  = f r a &&& f r b
-   f r (a :/: b)  = f r a &&& f (not r) b
+   f r (a :/: b)  = case a of -- two special cases (for efficiency)
+                       Nat 1          -> f (not r) b
+                       Negate (Nat 1) -> first not (f (not r) b)
+                       _              -> f r a &&& f (not r) b
    f r (Negate a) = first not (f r a)
    f r e          = (False, if r then (recip e:) else (e:))
    

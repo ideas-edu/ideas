1b84225bb5f9bbec65f5bf4aa2d65b22c63c8aa4
Josje Lodder <josje.lodder@ou.nl>
2010-06-18 09:30:01 +0000

    leuke voorbeeldopgaven!
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@3239 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Domain/Logic/Examples.hs
+++ src/Domain/Logic/Examples.hs
@@ -15,7 +15,32 @@ module Domain.Logic.Examples
    ( exampleProofs
    ) where
 
+
 import Domain.Logic.Formula
+import Common.Utils (ShowString(..))
+
+
+
+
 
 exampleProofs :: [(SLogic, SLogic)]
-exampleProofs = undefined
+exampleProofs = [(Not(p :||: (Not (p) :&&: q)), Not(p :||: q)),
+                ((p :->:q):||: Not (p), (p :->: q) :||: q),
+                ((p :&&: Not(q)):||:(q :||: Not(p)), (p :||:q):&&:Not(p :&&: q)),
+                (Not(p :||: Not(p :||: Not (q))), Not(p :||: q)),
+                (p :<->: q, (p :->: q) :&&: (q :->: p)),
+                ((p :&&: q) :->: p, T),
+                ((p :->: q) :||: (q :->: p), T),
+                ((q :->: (Not(p) :->: s)) :->: p, Not(p) :->: (q :&&: ((p :&&: q) :&&: q))),
+                ((p :->: Not(q)):->:q, (s :||:(s :->:(q :||: p))) :&&: q),
+                (p :->: (q :->: r), (p :->: q) :->: (p :->:r)),
+                (Not((p :->: q) :->: Not(q :->: p)), (p :<->: q)),
+                 (((p :->: q):->: (p :->: s)), (Not (q) :->: Not(p)) :->: (Not(s) :->: Not(p)))]
+ where
+   p = Var (ShowString "p")
+   q = Var (ShowString "q")
+   s = Var (ShowString "s")
+   r = Var (ShowString "r")
+
+
+

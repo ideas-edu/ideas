494df8fd620cc685adc3d14b8469c20b241322b9
Alex Gerdes <agerdes@me.com>
2011-02-24 14:27:05 +0000

    Removed prog. domain from the trunk
    git-svn-id: https://ideas.cs.uu.nl/svn/Feedback/trunk@3847 d9a34ac6-6904-0410-8848-93d71bbb9804

--- src/Domain/Programming/Loc.hs
+++
@@ -1,7 +0,0 @@
-module Domain.Programming.Loc where
-
-data Loc  =  Def String | App Int
-
-instance Show Loc where
-  show (Def s)  =  "`" ++ s ++ "'"
-  show (App n)  =  "labelled `" ++ show n ++ "'"
